2ce9e5d3f4dafe58a341c8d1bf74223a117d11ee
Hung <hunghaui2310@gmail.com>
2019-12-02 16:08:34 +0700

    commit

--- SpringbootAngular/backend/src/main/java/com/spring/angular/controller/ProductController.java
+++ SpringbootAngular/backend/src/main/java/com/spring/angular/controller/ProductController.java
@@ -1,15 +1,16 @@
 package com.spring.angular.controller;
 
+import com.spring.angular.dto.ProductDTO;
 import com.spring.angular.helper.ApiResponse;
 import com.spring.angular.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
+import java.util.List;
 
 @RestController
 @RequestMapping("product")
@@ -29,4 +30,14 @@ public class ProductController {
         }
     }
 
+    @GetMapping("/fakeData")
+    public List<ProductDTO> getFakeData(){
+        List<ProductDTO> list = productService.getAllProduct();
+        try{
+            return list;
+        }catch (Exception e){
+            e.printStackTrace();
+            return null;
+        }
+    }
 }
--- SpringbootAngular/backend/src/main/java/com/spring/angular/service/impl/ProductServiceImpl.java
+++ SpringbootAngular/backend/src/main/java/com/spring/angular/service/impl/ProductServiceImpl.java
@@ -1,6 +1,7 @@
 package com.spring.angular.service.impl;
 
 import com.spring.angular.dto.ProductDTO;
+import com.spring.angular.model.Product;
 import com.spring.angular.repository.ProductRepo;
 import com.spring.angular.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,29 +17,31 @@ public class ProductServiceImpl implements ProductService {
     @Autowired
     private ProductRepo productRepo;
 
-
     @Override
     public List<ProductDTO> getAllProduct() {
+        long lngId = 0;
         List<Object[]> lstObject = productRepo.getProduct();
         List<ProductDTO> productDTOList = new ArrayList<>();
-        ProductDTO productDTO = new ProductDTO();
+
         String proName = ""; int price =0;
         int numLike = 0; String discount = "";
         String img;
         for(Object[] objects : lstObject){
+            ProductDTO productDTO = new ProductDTO();
             proName = String.valueOf(objects[0]);
             price = (int) objects[1];
             numLike = (int) objects[2];
             discount = String.valueOf(objects[3]);
             img = String.valueOf(objects[4]);
 
+            productDTO.setId(lngId++);
             productDTO.setProductName(proName);
             productDTO.setPrice(price);
             productDTO.setNumLike(numLike);
             productDTO.setDiscount(discount);
             productDTO.setUrlImage(img);
+            productDTOList.add(productDTO);
         }
-        productDTOList.add(productDTO);
         return productDTOList;
     }
 }
