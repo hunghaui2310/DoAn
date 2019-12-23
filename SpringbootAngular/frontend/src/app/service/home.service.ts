092c8b5641be7c9490afe8624672917bfc170791
Hung <hunghaui2310@gmail.com>
2019-12-23 15:19:02 +0700

    commit

---
+++ SpringbootAngular/frontend/src/app/service/home.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {config} from '../../app-routing/application.config';
+import {SearchRequest} from '../../model/search.request';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HomeService {
+
+  constructor(private http: HttpClient) { }
+
+  search(searchModel: SearchRequest) {
+    return this.http.post(config.search_product, searchModel);
+  }
+}
