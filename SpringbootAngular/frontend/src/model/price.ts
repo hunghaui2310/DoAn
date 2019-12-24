8d2968056dc0b877a30c26ba71e42a01a73ce43f
Hung <hunghaui2310@gmail.com>
2019-12-24 13:51:07 +0700

    commit

---
+++ SpringbootAngular/frontend/src/model/price.ts
@@ -0,0 +1,23 @@
+export class Price {
+  value: number;
+  name: string;
+}
+
+const prices: Price[] = [
+  {
+    value: 1,
+    name: 'Dưới 10 triệu'
+  },
+  {
+    value: 2,
+    name: 'Từ 10 đến 15 triệu'
+  },
+  {
+    value: 3,
+    name: 'Từ 15 đến 20 triệu'
+  },
+  {
+    value: 4,
+    name: 'Trên 20 triệu'
+  }
+]
